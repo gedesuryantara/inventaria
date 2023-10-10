30562a0aebb48b5fffb58d33f3f8f1ec66a21e56
Nightqueen999 <niknanda999@gmail.com>
2023-10-10 21:03:39 +0800

    Hapus yang ga penting

--- app/views/about/index.php
+++
@@ -1,6 +0,0 @@
-
-<div class="container">
-    <h1 class="mt-4">Halaman About Me</h1>
-    <img src="<?= BASEURL; ?>/img/estetikBanget.jpeg"  alt="" width="200" height="auto" class="rounded-circle shadow">
-    <p>Hallo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah <?= $data['pekerjaan']; ?>.</p>
-</div>
--- app/views/about/page.php
+++
@@ -1,2 +0,0 @@
-
-    <h1>Hallo Cuy Halaman Page</h1>
