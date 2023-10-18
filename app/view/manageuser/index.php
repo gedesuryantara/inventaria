4ebc48b4418dbed80349250345511a376632504a
Lonely6475 <gdeadiwidyautama@gmail.com>
2023-10-18 11:19:45 +0800

    Menambahkan Form Confirm Password btn add users

--- app/view/manageuser/index.php
+++ app/view/manageuser/index.php
@@ -188,14 +188,20 @@
                         </div>
                         <div class="modal-body">
                             <div class="mb-3 row">
-                                <label for="Username" class="col-sm-2 col-form-label">Username</label>
-                                <div class="col-sm-10">
+                                <label for="Username" class="col-sm-3 col-form-label">Username</label>
+                                <div class="col-sm-9">
                                     <input type="text" class="form-control" id="Username">
                                 </div>
-                              </div>
-                              <div class="mb-3 row">
-                                <label for="Password" class="col-sm-2 col-form-label">Password</label>
-                                <div class="col-sm-10">
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="Password" class="col-sm-3 col-form-label">Password</label>
+                                <div class="col-sm-9">
+                                    <input type="password" class="form-control" id="Password">
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="Password" class="col-sm-3 col-form-label">Re-Password</label>
+                                <div class="col-sm-9">
                                     <input type="password" class="form-control" id="Password">
                                 </div>
                             </div>
