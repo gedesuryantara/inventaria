5d5e6d48f9e954de9ece3b92d6871922e9fec5d5
gedesuryantara <146906693+gedesuryantara@users.noreply.github.com>
2024-01-10 10:51:37 +0800

    Update Rak_model.php

--- app/model/Rak_model.php
+++ app/model/Rak_model.php
@@ -1,4 +1,7 @@
-class Rak_model {
+<?php
+
+class Rak_model
+{
     private $tabel = 'rak';
     private $db;
 
@@ -7,40 +10,39 @@ class Rak_model {
         $this->db = new Database;
     }
 
-    public function tambahRak($data){
+    public function tambahRak($data)
+    {
         $query = "INSERT INTO rak VALUES ('',:nama_rak,:jumlah_kolom) ";
 
         $this->db->query($query);
-        $this->db->bind('nama_rak',$data['namarak']);
-        $this->db->bind('jumlah_kolom',$data['jumlahkolom']);
+        $this->db->bind('nama_rak', $data['namarak']);
+        $this->db->bind('jumlah_kolom', $data['jumlahkolom']);
 
         $this->db->execute();
 
         return $this->db->rowCount();
     }
 
-    public function queryRak() 
+    public function queryRak()
     {
         $this->db->query("SELECT * FROM " . $this->tabel);
         return $this->db->resultSet();
     }
-    
-    public function queryselectbertingkat(){
+
+    public function queryselectbertingkat()
+    {
         $this->db->query("SELECT * FROM " . $this->tabel);
         $result = $this->db->resultSet();
-
         $data = [];
 
         foreach ($result as $row) {
-            // Lakukan sesuatu dengan data, misalnya, tampilkan di halaman web
             $rakId = $row['id_rak'];
             $rakNama = $row['nama_rak'];
             $jumlahKolom = $row['jumlah_kolom'];
 
             $data[] = array('id_rak' => $rakId, 'rak_nama' => $rakNama, 'jumlah_kolom' => $jumlahKolom);
-            // Anda bisa menyesuaikan sesuai dengan struktur tabel Anda
         }
 
-        echo json_encode(array('data'=> $data));
+        return json_encode(array('data' => $data));
     }
 }
