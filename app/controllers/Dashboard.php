61c5ceebc8b50a78832d06d663a4c3a19b0988b4
gedesuryantara <146906693+gedesuryantara@users.noreply.github.com>
2024-01-10 10:11:42 +0800

    Update Dashboard.php

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -1,48 +1,87 @@
-public function editbarang()
-{
-    $data['judul'] = 'Edit Barang';
-    $data['activeItem'] = 'active-item';
+<?php
 
-    $this->view('tamplates/header', $data);
-    $this->view('dashboard/editbarang', $data);
-    $this->view('tamplates/footer');
-}
-
-public function tambahBarang()
+class Login extends Controller
 {
-    if ($this->model('Barang_model')->tambahDataBarang($_POST) > 0) {
-        header('Location: ' . BASEURL . '/dashboard');
+    public function index()
+    {
+        session_start();
+
+        if (isset($_SESSION['userLogin'])) {
+            if ($_SESSION['userTrue'] !== true) {
+                $location = 'test';
+            } else {
+                $location = 'dashboard';
+            }
+            header('Location: ' . BASEURL . '/' . $location);
+        }
+
+        $data['judul'] = 'Login';
+        $_SESSION['status'] = [];
+
+        if (!empty($_SESSION['status'])) {
+            if ($_SESSION['status'] == 1) {
+                header('location: ' . BASEURL . '/manageuser');
+            } else {
+                header('location: ' . BASEURL . '/manageuser/useredit');
+            }
+        }
+
+        $this->view('tamplates/headerlogin', $data);
+        $this->view('login/index', $data);
+        $this->view('tamplates/footer');
+    }
+
+    public function logout()
+    {
+        session_start();
+        $_SESSION = [];
+        session_unset();
+        session_destroy();
+
+        header('location: ' . BASEURL . '/login');
+    }
+
+    public function user()
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if (!empty($username) || !empty($password)) {
+            if ($this->model('Login_model')->cekUserTrue($username) > 0) {
+                $data = $this->model('Login_model')->ambilDataUser($username);
+                $passwordDb = $data['password'];
+
+                if (password_verify($password, $passwordDb)) {
+                    session_start();
+
+                    $_SESSION['status'] = $data['status'];
+
+                    if ($_SESSION['status'] == 1) {
+                        header('location: ' . BASEURL . '/manageuser');
+                    } else {
+                        header('location: ' . BASEURL . '/manageuser/useredit');
+                    }
+
+                    $_SESSION['userLogin'] = 'success';
+
+                    if ($data['status'] != 0) {
+                        $_SESSION['userTrue'] = true;
+                        $location = "dashboard";
+                    } else {
+                        $_SESSION['userTrue'] = false;
+                        $location = "test";
+                    }
+                } else {
+                    echo 'password salah';
+                }
+            } else {
+                echo 'gagal menemukan user';
+            }
+        } else {
+            echo 'isikan data terlebih dahulu';
+        }
+
+        header('Location: ' . BASEURL . '/' . $location);
         exit;
     }
-}
-
-public function page($idPages)
-{
-    // pagination
-    $batasHalaman = 3;
-    $jumlah_data = count($this->model('Barang_model')->queryData());
-    $data['halaman_aktif'] = (isset($idPages)) ? (int)$idPages : 1;
-    $halamanAwal = ($batasHalaman * $data['halaman_aktif']) - $batasHalaman;
-    $data['jumlah_halaman'] = ceil($jumlah_data / $batasHalaman);
-
-    $data['judul'] = 'Dashboard';
-    $data['barang'] = $this->model('Barang_model')->getAllBarangPage($halamanAwal, $batasHalaman);
-    $data['rakData'] = $this->model('Rak_model')->queryRak();
-    $data['activeItem'] = 'active-item';
-
-    $this->view('tamplates/header', $data);
-    $this->view('dashboard/index', $data);
-    $this->view('tamplates/footer');
-}
-
-public function deleteBarang($id)
-{
-    if ($this->model('Barang_model')->deleteDataBarang($id)) {
-        header('Location: ' . BASEURL . '/dashboard');
-    }
-}
-
-public function selectbertingkat()
-{
-    $this->model('Rak_model')->queryselectbertingkat();
 }
